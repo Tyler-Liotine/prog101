7222d5ea2d9a0fed72aa179cbdfbe5d547223d6d
Tyler-Liotine <Tliotine@ucsb.edu>
2025-02-09 22:02:52 -0800

    prog101 Resubmit

--- exercises/prog101-expressions-variables.R
+++ exercises/prog101-expressions-variables.R
@@ -1,15 +1,25 @@
 # What do the following expressions do?
 temp_F <- temp_C * 9/5 - 32
+# Convert temperature in Celsius to Fahrenheit
+
 rect_area <- rect_length * rect_width
+# Finds the area of a rectangle
 
 # Fill in the question marks to complete these expressions
-????_area <- 1/2 * base * height
-cylinder_volume <- pi * ????^2 * ???
+triangle_area <- 1/2 * base * height
+cylinder_volume <- pi * radius^2 * height
 
-  # What are the errors in these expressions?
-  speed_m_s <- distance_m * time_s
+
+# What are the errors in these expressions?
+
+speed_m_s <- distance_m * time_s
+# Should be velocity_m_s not speed
+
 acceleration m s2 <- speed_m_s / time_s
+# Missing the underscores: acceleration_m_s^2
+
 force_N < mass + acceleration_m_s2
+# Mas does not have units and the squared is not raised to the power
 
 # The energy density of market squid is 4850 joules per gram[1]. If a 225 kg
 # Risso's dolphin needs 5 million joules of energy per day[2] and a typical
@@ -17,6 +27,12 @@ force_N < mass + acceleration_m_s2
 # eat each day? Solve the above word problem in R. Consider how many variables
 # you need to create, what to name them to make the code readable, and how to
 # combine them into expressions.
+density_J_g <- 4850
+squid_g <- 35
+total_J <- 5000000
+J_squid <- density_J_g * squid_g
+total_squid <- total_J / J_squid
+print(total_squid)
 
 # [1] Price et al. 2024 https://doi.org/10.3389/fmars.2023.1345525
 # [2] Barlow et al. 2008 https://doi.org/10.3354/meps07695
--- exercises/prog101-multiple-vectors.R
+++ exercises/prog101-multiple-vectors.R
@@ -4,14 +4,18 @@ instrument_deployed_hm <- c(730, 915, 1345)
 instrument_deployed_h <- floor(instrument_deployed_hm) / 100
 instrument_deployed_hdec <- (instrument_deployed_hm %% 100) / 60
 instrument_deployed <- instrument_deployed_h + instrument_deployed_hdec
+# Finds the total amount of time in which an instrument is deployed
 
 # Based on the code above, calculate the duration of instrument deployments
 # using the instrument recovery times below. What units do the durations have?
 instrument_recovered_hm <- c(1600, 1920, 2015)
-???
-instrument_recovered <- ??? + ???
-
-instrument_durations <- ??? - ???
+instrument_recovered_h <- floor(instrument_recovered_hm) / 100
+instrument_recovered_hdec <- (instrument_recovered_hm %% 100) /60
+instrument_recovered <- instrument_recovered_h + instrument_recovered_hdec
+instrument_durations <- instrument_recovered - instrument_deployed
+# They are in hours and minutes
 
 # Which site had the longest duration? Use conditional indexing.
 site <- c("Santa Cruz", "Santa Rosa", "San Miguel")
+longest_durations <- site[(instrument_recovered_hm) == max(instrument_recovered_hm)]
+print(longest_durations)
